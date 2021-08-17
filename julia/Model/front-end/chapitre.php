53fc7f8928698b25d960f8571fdc27ab11112a61
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2021-08-17 19:41:59 +0200

    Delete julia directory

--- julia/Model/front-end/chapitre.php
+++
@@ -1,9 +0,0 @@
-<?php
-class commentaire extends model{
-    $reponse = $bdd->query('SELECT * FROM chapitre');
-    echo $donnees['titre-last-chapitre']; 
-    echo $donnees['chaptirenumber'];
-    echo $donnees['chapitre-text'];
-    echo $donnees['chapitre-titre'];
-}
-?>
