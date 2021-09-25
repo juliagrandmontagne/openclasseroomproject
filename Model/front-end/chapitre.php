ac662c87283d0655320dabbb6e2844de5be609fa
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2021-09-25 13:38:57 +0200

    Add files via upload

--- Model/front-end/chapitre.php
+++ Model/front-end/chapitre.php
@@ -2,7 +2,7 @@
 class modelchapitre {
 public function connexionbdchapitre() {
 require('Model/front-end/connexion.php');
-$reponse = $bdd->query('SELECT * FROM Chapitre /*WHERE identite= ?*/');
+$reponse = $bdd->query('SELECT * FROM Chapitre /*WHERE id= ?*/');
 $reponse->execute();
         $data = $reponse->fetchAll();
         return $data;
--- view/front-end/chapitres.php
+++ view/front-end/chapitres.php
@@ -8,8 +8,7 @@
  <div id="chaptirenumber">
  <div id='chapitre-titre'><?php echo $donnees['chapitre-number']?><?php echo $donnees['chapitre-titre']?></div>
  <?php $identite=$donnees['ID']?>
-   <?php $_GET['identite']?>
- <div id="chapitre-text"><?php echo $donnees['chapitre-text']?><br><li id="suite"><a id="suite"href="index.php?action=chapitre&.<?php echo $identite?>=?">lire la suite</a></li> 
+ <div id="chapitre-text"><?php echo $donnees['chapitre-text']?><br><li id="suite"><a id="suite"href="index.php?action=chapitre&id=<?php echo "$identite"?>">lire la suite</a></li> 
   <img src="<?php echo $donnees['images']?>" alt="illustrationchapitre" id="illustrationchapitre"> </div>
  </div><?php endforeach; ?></div>
 
