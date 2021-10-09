56d0670da086f117a7a634718d13d2f78534ad07
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2021-10-09 17:14:55 +0200

    Add files via upload

--- view/front-end/contact.php
+++ view/front-end/contact.php
@@ -1,8 +1,32 @@
 <?php $title = 'contact' ; ?>
 <?php ob_start() ; ?>
+<?php 
+//traintement données
+if(!empty($_post))
+//verification des données presente
+if(
+    isset($_post["user_name"]), ($_post["user_mail"]), ($_post["user_firstname"]), ($_post["user_message"]) && !empty($_post)["user_name"] && !empty($_post)["user_mail"] && !empty($_post["user_firstname"]) && !empty($_post["user_message"])
+)
+{   $user_name = htmlspecialchars($_post["user_name"]);
+    $user_mail = htmlspecialchars($_post["user_mail"]);
+     $user_firstname = htmlspecialchars($_post["user_firstname"]);
+    $user_messag = htmlspecialchars($_post["user_message"]);
+     $sql = "INSERT INTO contact(user_name, user_mail, user_firstname, user_message
+       VALUES(:user_name, :user_mail, :user_firstname, :user_message, )");
+      require('Model/front-end/connexion.php');
+       $sth= $bdd->prepare($sql);
+      $sth->bindValue(':user_name',$user_name, PDO::PARAM_STR);
+       $sth->bindValue(':user_mail',$user_mail, PDO::PARAM_STR);
+      $sth->bindValue(':user_firstname',$user_firstname,PDO::PARAM_STR);
+      $sth->bindValue(':user_message',$user_message,PDO::PARAM_STR);
+ 
+}
+else (echo"le formulaire est incomplet" )
+
+?>
 
         <div class="form">
-            <form action="traitement.php" method="post" >
+            <form  method="post" >
             <fieldset class="info">
                 <legend>Nous contacter</legend>
             </fieldset>
