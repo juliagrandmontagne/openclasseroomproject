aea5b2e0a6cd8ad830a75a59c39688770e986075
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2021-10-19 17:01:53 +0200

    Add files via upload

--- view/front-end/contact.php
+++ view/front-end/contact.php
@@ -2,40 +2,38 @@
 <?php ob_start() ; ?>
 <?php 
 //traintement données
-    //if(!empty($_post))
 //verification des données presente
-    //if(
-    //isset($_post["user_name"]), ($_post["user_mail"]), ($_post["user_firstname"]), ($_post["user_message"]) && !empty($_post)["user_name"] && !empty($_post)["user_mail"] && !empty($_post["user_firstname"]) && !empty($_post["user_message"])
+//if(
+   // isset($_post["user_name"],$_post["user_mail"],$_post["user_firstname"],$_post["user_message"]) && !empty($_post["user_name"]) && !empty($_post["user_mail"]) && !empty($_post["user_firstname"]) && !empty($_post["user_message"])
 //)
-   $user_name = $_post["user_name"];
-    $user_mail = $_post["user_mail"];
-    $user_firstname = $_post["user_firstname"];
-    $user_message =$_post["user_message"];
-    try  {
-        $bdd=new PDO('mysql:host=localhost;dbname=project4;charset=utf8', 'root', 'root') ;
-        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sth= $bdd->prepare("INSERT INTO contact(user_name, user_mail, user_firstname, user_message)
-        VALUES(:user_name, :user_mail, :user_firstname, :user_message )");
+	$user_name = $_POST["user_name"];
+    $user_mail = $_POST["user_mail"];
+    $user_firstname = $_POST["user_firstname"];
+    $user_message = $_POST["user_message"];
+ 
+ 	try  {
+    	$bdd=new PDO('mysql:host=localhost;dbname=project4;charset=utf8', 'root', 'root') ;
+		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-         $sth->bindParam(':user_name',$user_name);
-         $sth->bindParam(':user_mail',$user_mail);
-         $sth->bindParam(':user_firstname',$user_firstname);
-         $sth->bindParam(':user_message',$user_message);
-         $sth->execute();
-         //header("Location:index.php")
-    }
-    catch (PDOException $e) {
-        echo "Error!: " . $e->getMessage() . "<br/>";
-         
-          }
-   
-
-//else (echo"le formulaire est incomplet" )
+ 	$sth= $bdd->prepare("INSERT INTO contact(user_name, user_mail, user_firstname, user_message)
+ VALUES(:user_name, :user_mail, :user_firstname, :user_message )");
+       
+     $sth->bindParam(':user_name',$user_name);
+     $sth->bindParam(':user_mail',$user_mail);
+     $sth->bindParam(':user_firstname',$user_firstname);
+     $sth->bindParam(':user_message',$user_message);
+     $sth->execute();
+		header("Location:index.php");
+	}
+	catch (PDOException $e) {
+	echo "Error!: " . $e->getMessage() . "<br/>";
+  	}
+// else echo"le formulaire est incomplet" ;
 
 ?>
 
         <div class="form">
-            <form  action="index.php?action=contact" method="post" enctype="multipart/form-data">
+            <form action="index.php?action=contact" method="POST" enctype="multipart/form-data" >
             <fieldset class="info">
                 <legend>Nous contacter</legend>
             </fieldset>
@@ -46,15 +44,18 @@
             </legend>
             </fieldset>
                 <label for="user_name">Nom</label>
-                <input type="text" class="name" name="user_name" placeholder="Nom" required>
-                <label for="user_mail">Nom</label>
-                <input type="text" id="mail" name="user_mail" placeholder="Email" required>
-                <label for="user_firstname">Nom</label>
-                <input type="text" id="prenom" name="user_firstname" placeholder="Objet" required >
-                <label for="user_message">Nom</label>
-                <textarea type="text" id="msg" name="user_message" placeholder="Message" required></textarea>
-
-                <button class="send" type="submit" id="btn-submit" >Envoyer</button>
+                <input type="text" class="name" name="user_name" id="user_name" required>
+                
+                <label for="mail">E-mail</label>
+				<input type="mail" name="user_mail" id="mail" required>
+            
+                <label for="user_firstname">Prénom</label>
+				<input type="text" name="user_firstname" id="user_firstname" required>
+            	
+				<label for="msg">Message</label>
+                <textarea id="msg" name="user_message" type="text" required></textarea>
+				
+				<button class="send" type="submit" id="btn-submit">Envoyer</button>
             
             </form>
         </div>
