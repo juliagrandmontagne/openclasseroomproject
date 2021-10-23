0d86773aefdabb2b1361700035f8738b30c956cc
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2021-10-23 21:53:53 +0200

    Add files via upload

---
+++ Model/front-end/controller/front-end/controller.php
@@ -0,0 +1,36 @@
+<?php
+
+function directionaccueil()
+{ 
+require('Model/front-end/accueil.php');
+$instanceclasse2 = new modelaccueil();
+$databd2 = $instanceclasse2-> connexionbdaccueil() ;
+require('view/front-end/accueil.php');
+}
+function directionchapitre()
+   {
+require('Model/front-end/chapitre.php');
+$instanceclasse = new modelchapitre();
+$databd = $instanceclasse-> connexionbdchapitre();
+$databd3 = $instanceclasse-> connexionbdcommentaire();
+require('view/front-end/chapitre.php');
+   }
+
+function directionchapitres()
+   {
+require('Model/front-end/chapitres.php');
+$instanceclasse = new modelchapitres();
+$databd = $instanceclasse-> connexionbdchapitres();
+require('view/front-end/chapitres.php');
+   }
+    function directionauteur()
+    { 
+        require('view/front-end/auteur.php');
+    }
+    function directioncontact()
+{
+   $instanceclasse4 = new modelcontact ();
+$databd4 = $instanceclasse4-> connexionbdcontact();
+    require('view/front-end/contact.php');
+}
+?>
