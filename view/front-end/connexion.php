b0ccefff9ce5380e7f92ed5837976fcfa0e33bc1
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2022-01-06 21:44:09 +0100

    Add files via upload

--- view/front-end/connexion.php
+++ view/front-end/connexion.php
@@ -1,15 +1,31 @@
 <?php $title = 'connexion' ; ?>
 <?php ob_start() ; ?>
+<?php 
+if (isset($_POST['valider']))
+{
+if(!empty($_POST['nameuser']) AND !empty($_POST['mp']) ){
+    $nameadmin = "jeanforteroche";
+    $mpadmin = "alohomora";
+
+    $pseudosaisie = htmlspecialchars($_POST['nameuser']);
+    $mpsaisie = htmlspecialchars($_POST['mp']);
+    if($pseudosaisie == $nameadmin AND $mpsaisie == $mpadmin) {
+        header('Location: index.php?action=tableaudebord');
+    }
+    }else{ echo"mot de passe incorrecte ";}
+}else{ echo"veuillez completer tous les champs ";
+}
+?>
 <section class="connexion">
 <h3 id="messageconnexion">Vous êtes un utilisateur ? Alors connectez vous.</h3>
 <div id="formulaire">
- <form action="index.php?action=tableaudebord" method="post" id="connexionutilisateur" >     
+ <form action="" method="post" id="connexionutilisateur" >     
                 <div id="titre-commentaire"><legend ></legend></div>
                 <input class="nameuser" name="nameuser" placeholder="pseudo" required>
             
-                <textarea id="comment" name="mp" placeholder="mot de passe" required></textarea>
+                <input id="comment" type="pasword" name="mp" placeholder="mot de passe" required>
 
-                <input class="send" type="submit" id="btn-submit" value="Envoyer" >
+                <input class="send" type="submit" id="btn-submit" value="Envoyer" name="valider">
             
             </form>
 </div>
