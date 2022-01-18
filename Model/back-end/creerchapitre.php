adaa4f1d51c027a9df43cf4e33c5ecbc9e91b83e
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2022-01-18 07:42:45 +0100

    Add files via upload

--- Model/back-end/creerchapitre.php
+++ Model/back-end/creerchapitre.php
@@ -5,9 +5,10 @@ class modelcreerchapitre
     {
       require('Model/front-end/connexion.php');
       $sth = $bdd->prepare('INSERT INTO chapitre (chapitrenumber, chapitretitre , resumer,	chapitretext , brouillon, images) VALUES(?, ?, ?, ?, ?, ?)');
-      $sth->execute(array($_POST['numero'], $_POST['name'],$_POST['resume'], $_POST["histoire"],1,"images/neige-illustration.jpg"));   
+      $sth->execute(array($_POST['numero'], $_POST['titre'],$_POST['resume'], $_POST["histoire"],$_POST["select"],"0"));   
     
     }
+    
   }     
 
 ?>
--- view/back-end/creerchapitre.php
+++ view/back-end/creerchapitre.php
@@ -17,14 +17,40 @@
     
 				<label class="ecrire" for="histoire">Texte</label>
                 <textarea id="histoire" name="histoire" type="text" required></textarea>
-              
-           
-				
+                
+                <label for="file">Image</label>
+                <input type="file" name="file">
+                
+                <select name="select" id="select">
+                <option value="0">publier</option>
+                <option value="1">brouillon</option>
+                </select>
  
 				<button class="send" type="submit" id="btn-submit">Envoyer</button>
             
             </form>
         </div>
-</section>
+     
+<?php
+if(isset($_FILES['file'])){
+  $tmpName = $_FILES['file']['tmp_name'];
+  $name = $_FILES['file']['name'];
+  $size = $_FILES['file']['size'];
+  $error = $_FILES['file']['error'];
+}
+move_uploaded_file($tmpName, './images/'.$name);
+$tabExtension = explode('.', $name);
+$extension = strtolower(end($tabExtension));
+//Tableau des extensions que l'on accepte
+$extensions = ['jpg', 'png', 'jpeg', 'gif'];
+if(in_array($extension, $extensions)){
+    move_uploaded_file($tmpName, './upload/'.$name);
+}
+else{
+    echo "Mauvaise extension";
+}
+?>
+       
+  </section>
 <?php $content = ob_get_clean() ; ?>
 <?php include('templateadminagain.php'); ?>
