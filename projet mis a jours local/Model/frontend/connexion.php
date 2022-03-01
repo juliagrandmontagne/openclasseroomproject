f14c8bc2703313e1e139ef97f4e048aac24a567c
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2022-03-01 21:09:51 +0100

    Delete projet mis a jours local directory

--- projet mis a jours local/Model/frontend/connexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-try  {
-    $bdd=new PDO('mysql:host=localhost;dbname=project4;charset=utf8', 'root', 'root') ;
-  }
-catch (PDOException $e) {
-echo "Error!: " . $e->getMessage() . "<br/>";
-    die();
-  }
-?>
