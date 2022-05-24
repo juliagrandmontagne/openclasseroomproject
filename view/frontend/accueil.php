9f871e1beedd38909f58e853561cd445d5deaed6
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2022-05-24 12:20:55 +0200

    Add files via upload

--- view/frontend/accueil.php
+++ view/frontend/accueil.php
@@ -9,7 +9,7 @@
 			   <div id="description"><p id="descriptionacueil">Bonjour et bienvenue sur le site de Jean Forteroche.<br>Vous trouverez sur ce site les derniers chapitres des aventures de Louise en Alaska....<br><br>Vous trouverez aussi la biographie de l'auteur ainsi que son contact pour toute suggestion professionnelle.<br>Nous espérons que vous aprécierez ce récit..</div>
 			   <?php  foreach ($databdaccueil as $donnees) : ?>
 			   <div id="lastchapter"><h3>Le dernier chapitre : <?php echo $donnees['chapitretitre']?><?php echo $donnees['chapitrenumber']?></h3><?php echo $donnees['resumer']?>
-			   <img src="<?php echo $donnees['images']?>" alt="illustrationchapitre" id="illustrationchapitre"> 
+			   <img src="<?php echo $donnees['images']?>" alt="illustrationchapitre" class="illustrationchapitre"> 
 			   <?php $identite=$donnees['ID']?>
 			   <div id="btn-submit"><a id="suite" href="index.php?action=chapitre&id=<?php echo "$identite"?>">lire la suite</a></div></div>
 				<?php endforeach; ?>
