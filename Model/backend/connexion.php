3af76e2f57f0805c57c179a8a92aabbd0f3aa11a
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2022-06-26 20:22:48 +0200

    Add files via upload

---
+++ Model/backend/connexion.php
@@ -0,0 +1,13 @@
+<?php
+try  {
+  //Local
+  $bdd=new PDO('mysql:host=localhost;dbname=project4;charset=utf8', 'root', 'root') ;
+  //online
+  //
+  //$bdd=new PDO('mysql:host=db5006311013.hosting-data.io;dbname=dbs5268898;charset=utf8', 'dbu402848', 'ceciestunmotdepasse!') ;
+  }
+catch (PDOException $e) {
+echo "Error!: " . $e->getMessage() . "<br/>";
+    die();
+  }
+?>
